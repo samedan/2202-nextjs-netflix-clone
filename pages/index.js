e30072a62fb96ad6158dc40ca3101ab2f361ce0a
Daniel Popescu <same.dan@gmail.com>
2022-03-02 15:27:43 +0100

    Popular API Videos

--- pages/index.js
+++ pages/index.js
@@ -4,15 +4,25 @@ import NavBar from "../components/nav/navbar";
 import styles from "../styles/Home.module.css";
 import Banner from "./../components/banner/banner";
 import SectionCards from "./../components/card/section-cards";
-import { getVideos } from "../lib/videos";
+import { getPopularVideos, getVideos } from "../lib/videos";
 
 // on Server Side
 export async function getServerSideProps() {
-  const disneyVideos = await getVideos();
-  return { props: { disneyVideos } };
+  const disneyVideos = await getVideos("disney trailer");
+  const productivityVideos = await getVideos("productivity");
+  const travelVideos = await getVideos("travel");
+  const popularVideos = await getPopularVideos("disney trailer");
+  return {
+    props: { disneyVideos, productivityVideos, travelVideos, popularVideos },
+  };
 }
 
-export default function Home({ disneyVideos }) {
+export default function Home({
+  disneyVideos,
+  travelVideos,
+  productivityVideos,
+  popularVideos,
+}) {
   return (
     <div className={styles.container}>
       <Head>
@@ -21,16 +31,24 @@ export default function Home({ disneyVideos }) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      {/* <h1>Netflix</h1> */}
-      <NavBar username="dan@curuletz.com" />
-      <Banner
-        title="Clifford the red dog"
-        subTitle="a very cute dog"
-        imgUrl="/static/clifford.webp"
-      />
-      <div className={styles.sectionWrapper}>
-        <SectionCards title="Disney" videos={disneyVideos} size="large" />
-        <SectionCards title="Medium" videos={disneyVideos} size="medium" />
+      <div className={styles.main}>
+        {/* <h1>Netflix</h1> */}
+        <NavBar username="dan@curuletz.com" />
+        <Banner
+          title="Clifford the red dog"
+          subTitle="a very cute dog"
+          imgUrl="/static/clifford.webp"
+        />
+        <div className={styles.sectionWrapper}>
+          <SectionCards title="Disney" videos={disneyVideos} size="large" />
+          <SectionCards title="Travel" videos={travelVideos} size="small" />
+          <SectionCards
+            title="Productivity"
+            videos={productivityVideos}
+            size="medium"
+          />
+          <SectionCards title="Popular" videos={popularVideos} size="small" />
+        </div>
       </div>
     </div>
   );
