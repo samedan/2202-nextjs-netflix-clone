9dc4ba674989b8acd83ca70df4850d4ab0878d21
Daniel Popescu <same.dan@gmail.com>
2022-04-12 15:10:18 +0200

    Fixed an import

--- pages/video/[videoId].js
+++ pages/video/[videoId].js
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 
 import { useRouter } from "next/router";
 import Modal from "react-modal";
-import styles from "../../styles/Video.module.css";
+import styles from "../../styles/video.module.css";
 
 import NavBar from "../../components/nav/navbar";
 import clsx from "classnames";
